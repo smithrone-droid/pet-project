116a6ca3bf5d4966e3bb87decc2fa7d2686c4799
smitty <smitator@gmail.com>
2026-04-23 15:13:49 +0300

    add assertion in pageobject

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,22 @@
-class BasePage():
+from selenium.common import NoSuchElementException
+
+
+class BasePage:
     def __init__(self, browser, url):
         self.browser = browser
         self.url = url
 
     def open(self):
         self.browser.get(self.url)
+
+    def __init__(self, browser, url, timeout=10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(by=how, value=what)
+        except NoSuchElementException:
+            return False
+        return True
