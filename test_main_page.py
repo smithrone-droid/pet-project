de6cc099f08c004262ab1b74c2c036ba099e6b3e
smitty <smitator@gmail.com>
2026-04-23 14:35:01 +0300

    adding black formatter

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,18 @@
 from pages.main_page import MainPage
 
-#pytest -v --tb=line --language=en test_main_page.py
+
+# pytest -v --tb=line --language=en test_main_page.py
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(
+        browser, link
+    )  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()
     page.go_to_login_page()
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
